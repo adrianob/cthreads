0cac321bcc728533f7b2cad4bec3c68d27486635
lvalandro <lvalandrorocha@hotmail.com>
2016-09-28 21:04:51 -0300

    fixing string.h

--- src/cthreads.c
+++ src/cthreads.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-#inculde <string.h>
+#include <string.h>
 #include "../include/support.h"
 #include "../include/cthread.h"
 #include "../include/cdata.h"
